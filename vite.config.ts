2f67621308845a55b6df5373d0fc35e1a909a028
pixelandprose2026-stack <pixelandprose2026@gmail.com>
2026-06-28 15:12:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
+
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [inspectAttr(), react()],
+  base: '/',
+  plugins: [react()],
   server: {
     port: 3000,
   },
